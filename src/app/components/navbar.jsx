e277d69e57a8b2e4e3429896351f28b382ec4eba
frank117-alt <frankninja71@gmail.com>
2024-05-25 19:39:12 -0500

    actualizacion

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -175,6 +175,12 @@ export default function Navbar() {
                   >
                     objetivos especificos
                   </a>
+                  <a
+                    href="/galeria"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    objetivos especificos
+                  </a>
 
                   {company.map((item) => (
                     <a
